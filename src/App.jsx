c0553a9145648932c48c6b9bbab39376d4f63fb3
YesmineHaddouk <yesmine.haddouk@tbs.u-tunis.tn>
2026-02-05 15:26:47 +0100

    Week 2: First React component with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const courseTitle="React";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const student={
+  name:"Yesmine",
+  age:21,
+  track:"Development"
+}
+function sayHello(){
+  return 'Hello,${studentName}!';
 }
 
+function App() {
+  const studentName="Yesmine" ;
+return (
+  <div>
+      <h1> Yesmine's first element </h1>
+      <p>Student Name: {studentName}</p>
+      <p>Course: {courseTitle}</p>
+      <p>Welcome to {courseTitle}, {studentName}!</p>
+      <label htmlFor="nameInput">Your Name:</label>
+      <input id="nameInput" type="text" />
+      <p>Name: {student.name}</p>
+      <p>Age: {student.age}</p>
+      <p>Track: {student.track}</p>
+      <p>{sayHello()}</p>
+      <p>I understand how JSX allows me to combine JavaScript with HTML</p>
+      <p>Nothing, i understand everything</p>
+      <p>Where to define a function</p>
+  </div>
+);
+}
 export default App
